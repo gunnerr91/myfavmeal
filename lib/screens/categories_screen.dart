55157c58e19056544c0005f5a66bd1c1a385b77f
gunnerr91 <blissvortex@hotmail.com>
2020-07-02 21:13:48 +0100

    added bottom navigation bar

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -7,6 +7,7 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
+      padding: EdgeInsets.all(10),
       children: DUMMY_CATEGORIES.map((e) {
         return CategoryItem(e.id, e.title, e.color);
       }).toList(),
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,34 +8,75 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, dynamic>> _pages = [
+    {"page": CategoriesScreen(), "title": "MealMealMeals"},
+    {"page": FavoriteScreen(), "title": "MyFavMeal"}
+  ];
+  int _selectedPageIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Meals"),
-          bottom: TabBar(
-            indicatorColor: Colors.white70,
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-                text: 'Favorites',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]["title"]),
+      ),
+      body: _pages[_selectedPageIndex]["page"],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Theme.of(context).primaryColorLight,
+        selectedItemColor: Colors.white,
+        currentIndex: _selectedPageIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedPageIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.shifting,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text("Category"),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoriteScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.favorite),
+            title: Text("Favorites"),
+          ),
+        ],
       ),
     );
   }
 }
+
+// class _TabsScreenState extends State<TabsScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 2,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Meals"),
+//           bottom: TabBar(
+//             indicatorColor: Colors.white70,
+//             tabs: <Widget>[
+//               Tab(
+//                 icon: Icon(Icons.category),
+//                 text: 'Categories',
+//               ),
+//               Tab(
+//                 icon: Icon(Icons.favorite),
+//                 text: 'Favorites',
+//               ),
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: <Widget>[
+//             CategoriesScreen(),
+//             FavoriteScreen(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
